7bdf227d68f689507e958a56391c20913b131cf0
mugenzen <mugen8.tech@gmail.com>
2025-10-31 15:35:43 +0700

    Update page.tsx

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
       <ul style={{marginTop:12, lineHeight:2}}>
         <li><Link href="/members">Daftarkan Member & Generate Voucher</Link></li>
         <li><Link href="/vouchers">Riwayat Voucher & Proses</Link></li>
+        <li><Link href="/denominations">Allowed Denominations (Cash & Dummy)</Link></li>
       </ul>
     </main>
   );
